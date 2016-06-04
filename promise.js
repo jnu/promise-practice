56f006a907d6bad986ef6f525555cb38d459850e
Joe Nudell <joen@dropbox.com>
2016-06-04 12:27:30 -0700

    initial commit

---
+++ promise.js
@@ -0,0 +1,110 @@
+/**
+ * @class Custom Promise implementation.
+ *
+ * This implementation should pass tests in promise.spec.js.
+ */
+class Promise {
+
+    /**
+     * Construct a Promise. The constructor is given an `executor` function,
+     * which itself takes a `resolve` and a `reject` function as arguments.
+     *
+     * @example
+     *   new Promise((resolve, reject) => {
+     *     setTimeout(() => resolve('hello world!'), 1000);
+     *   });
+     *
+     * @example
+     *   new Promise((resolve, reject) => {
+     *     setTimeout(() => reject('goodbye cruel world'), 1000);
+     *   });
+     *
+     * @param {(resolve: (val: any) => void, reject: (reason: any) => void) => void} executor
+     */
+    constructor(executor) {
+        /** TODO Implement */
+    }
+
+    /**
+     * Register a callback when `resolve` is called
+     * @param  {(val: any) => any} callback
+     * @return {Promise}
+     */
+    then(onResolve, onReject) {
+        /** TODO Implement */
+    }
+
+    /**
+     * Handle Promise rejection, or uncaught errors in promise execution
+     * @param  {(reason: any) => any} callback
+     * @return {Promise}
+     */
+    catch(onReject) {
+        /** TODO Implement */
+    }
+
+    /**
+     * Execute a function regardless of whether the Promise resolved or rejected.
+     * Do not change its return value.
+     *
+     * Angular's $q implements this and it is sometimes useful. It is not in
+     * the actual ES2015 spec.
+     *
+     * @name Promise#finally
+     * @param {(val: any) => void} callback
+     * @return {Promise}
+     */
+
+}
+
+/**
+ * Construct a Promise in the resolved state
+ * @static
+ * @param  {any} value
+ * @return {Promise}
+ */
+Promise.resolve = function resolve(value) {
+    /** TODO Implement */
+};
+
+/**
+ * Construct a Promise in the rejected state
+ * @static
+ * @param  {any} reason
+ * @return {Promise}
+ */
+Promise.reject = function reject(reason) {
+    /** TODO Implement */
+};
+
+/**
+ * Construct a Promise that resolves when all input values are resolved, or
+ * rejects immediately if any reject. The Promise resolves with an array of
+ * values corresponding in order to the iterable passed as input (not the order
+ * in which they were resolved).
+ *
+ * If any input value is not a promise, it will be coerced to a Promise using
+ * Promise.resolve.
+ *
+ * @static
+ * @param  {Array<any>} iterable
+ * @return {Promise} - resolves with array corresponding to input promises
+ */
+Promise.all = function all(iterable) {
+    /** TODO Implement */
+};
+
+/**
+ * Construct a Promise that resolves or rejects with the first of a set of
+ * Promises, with the same value or reason.
+ * @name Promise.race
+ * @static
+ * @param {Array<Promise>} iterable
+ * @return {Promise}
+ */
+Promise.race = function race(iterable) {
+    /** TODO Implement */
+}
+
+
+export default Promise;
